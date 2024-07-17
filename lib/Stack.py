299cfdfe68029fa6fd92be18fe10af5d46ed3422
YUERONG HUANG <rowanne.h@hotmail.com>
2024-07-17 22:06:09 +1000

    complete lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,40 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        if len(self.items) == 0: 
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        return self.items
+        
 
     def pop(self):
-        pass
+        if len(self.items)>0:
+            return self.items.pop()        
+    
 
     def peek(self):
-        pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit: 
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        for i in range(0, len(self.items)) :
+            if (self.items[i]==target):
+                return len(self.items)-i-1
+        return -1
